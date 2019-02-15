e86993f47ed95188975c3ce6d83a10f3587515b5
Divine Dube <216004854@student.uj.ac.za>
2019-02-15 13:36:39 +0200

    add access permission

--- Songhound/ViewController.swift
+++ Songhound/ViewController.swift
@@ -7,20 +7,48 @@
 //
 
 import UIKit
-
+import GoogleMaps
 class ViewController: UIViewController {
+    
+    private let locationManager = CLLocationManager()
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-
-
-    }
-
-
-    func getCurrentLoacation() -> Void{
-
+        
+        locationManager.delegate = self as? CLLocationManagerDelegate
+        locationManager.requestWhenInUseAuthorization()
     }
 
 }
+
+
+// READ:  make sure the is conforms to the CLLocationManagerDelegate
+extension ViewController : CLLocationManagerDelegate {
+    func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
+        guard status == .authorizedWhenInUse else {
+            return
+        }
+        
+        locationManager.startUpdatingLocation()
+        
+    }
+    
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        guard let location = locations.first else {
+            // could not get the current location
+            print("could not get the current location")
+            return
+        }
+        
+        print("we know thw current location yoh \(location)")
+        
+        
+        
+        
+        locationManager.stopUpdatingLocation()
+    }
+}
+
 
