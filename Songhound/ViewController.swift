073f268b5af2f83b95156c9e71258d5709f85c65
Divine Dube <216004854@student.uj.ac.za>
2019-02-15 10:59:13 +0200

    Initial Commit

---
+++ Songhound/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/02/15.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
