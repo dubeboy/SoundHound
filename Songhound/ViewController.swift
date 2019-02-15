f7549d8afeb0fbb8b4ce4684dcf022b33f07b9c3
Divine Dube <216004854@student.uj.ac.za>
2019-02-15 15:13:56 +0200

    Get location by street name
    *Translate coordinate system to street names because street names are
    easily readable to humans ;)

--- Songhound/ViewController.swift
+++ Songhound/ViewController.swift
@@ -12,7 +12,6 @@ class ViewController: UIViewController {
     
     private let locationManager = CLLocationManager()
 
-
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,31 +20,49 @@ class ViewController: UIViewController {
         print("assigning the self to the delegate")
         locationManager.requestWhenInUseAuthorization()
     }
+    
+    private func reverseGeocoderCoordinates(_ coordinates: CLLocationCoordinate2D) {
+        
+        let geocoder = GMSGeocoder()
+        
+        
+        
+        //the closure is a callback because this does niot exec in the main thread
+        geocoder.reverseGeocodeCoordinate(coordinates) {response, error in
+            // powerful stuff yoh
+            guard let address = response?.firstResult(), let lines = address.lines else {
+                return
+            }
+            let fullAddress = lines.joined(separator: "\n")
+            print("the full address is \(fullAddress)")
+        }
+    }
 
 }
 
+//
+// EXTENTIONS
+//
 
 // READ:  make sure the is conforms to the CLLocationManagerDelegate
-extension ViewController : CLLocationManagerDelegate {
+ extension ViewController : CLLocationManagerDelegate {
     func locationManager(_ manager: CLLocationManager, didChangeAuthorization status: CLAuthorizationStatus) {
         guard status == .authorizedWhenInUse else {
             return
         }
-        
         locationManager.startUpdatingLocation()
-        
     }
     
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
         guard let location = locations.first else {
             // could not get the current location
-            print("could not get the current location")
+            print("sorry man could not get the current location")
             return
         }
+        print("we know the current location yoh \(location)")
         
-        print("we know thw current location yoh \(location)")
-        
-        
+        reverseGeocoderCoordinates(location.coordinate)
+        // use GMSGeocoder to get the address of the user yeah?
         locationManager.stopUpdatingLocation()
     }
 }
