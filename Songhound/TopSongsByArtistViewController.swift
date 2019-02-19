835fc37502e8ce30a1100eea8518bb9fb0a1b0ab
Divine Dube <216004854@student.uj.ac.za>
2019-02-19 11:12:53 +0200

    Add show all artists screen
    * able to see more artists in a list so that the user can manually search by artist

--- Songhound/TopSongsByArtistViewController.swift
+++ Songhound/TopSongsByArtistViewController.swift
@@ -8,15 +8,49 @@
 
 import UIKit
 
-class TopSongsByArtistViewController: UIViewController {
-
+class TopSongsByArtistViewController: UIViewController, UITableViewDataSource, UITableViewDelegate {
+    
+    @IBOutlet weak var artistsTableView: UITableView!
+    var artists: [Artist] = []
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+        populateArtists()
+        self.artistsTableView.delegate = self
+        self.artistsTableView.dataSource = self
 
         // Do any additional setup after loading the view.
     }
     
+    func populateArtists() {
+        for i in 1...10 {
+          let isItHot = i % 2 == 0 || i % 3 == 0
+            artists.append(Artist(name: "John \(i)", numHits: i, isHot: isItHot ))
+        }
+    }
+    
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return artists.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = self.artistsTableView.dequeueReusableCell(withIdentifier: "artistTableViewCell", for: indexPath) as! ArtistTableViewCell
+        
+        cell.lblArtistName.text = artists[indexPath.row].name
+        cell.lblNumHits.text = "\(artists[indexPath.row].numHits) hot songs"
+        let str = artists[indexPath.row].isHot ? "🔥" :  "";
+        cell.lblEmoji.text = str
+        
+//        cell.imgArtist. = // set the artist image 
+        
+        return cell
+    }
+    
+    
 
+    @IBAction func onBackButtonClick(_ sender: UIButton) {
+        dismiss(animated: true, completion: nil)
+    }
     /*
     // MARK: - Navigation
 
