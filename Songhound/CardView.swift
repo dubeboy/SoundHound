810efe730f601a5c965307b9ce3909d27ac5a4cf
Divine Dube <216004854@student.uj.ac.za>
2019-02-20 15:20:15 +0200

    create custom CustomViewCell

---
+++ Songhound/CardView.swift
@@ -0,0 +1,32 @@
+//
+//  CardView.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/02/20.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import UIKit
+
+class CardView: UIView {
+    @IBOutlet weak var labelArtistName: UILabel!
+    @IBOutlet weak var lblArtistName: UILabel!
+    
+    required init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        initSubviews()
+    }
+    
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        initSubviews()
+    }
+    
+    func initSubviews() {
+        let nib = UINib(nibName: "CaptionableImageView", bundle: nil)
+        nib.instantiate(withOwner: self, options: nil)
+        contentView.frame = bounds
+        addSubview(contentMode)
+        
+    }
+}
