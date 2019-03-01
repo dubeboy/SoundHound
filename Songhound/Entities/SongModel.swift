d2f4891671f5f6215ce7792718e60cabed9ec24c
Divine Dube <216004854@student.uj.ac.za>
2019-03-01 14:43:57 +0200

    Add VIPER protocols

---
+++ Songhound/Entities/SongModel.swift
@@ -0,0 +1,13 @@
+//
+//  SongModel.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/03/01.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import Foundation
+
+class SongModel {
+    
+}
---
+++ Songhound/SongsList/Presenter/SongsListViewPresenter.swift
@@ -0,0 +1,38 @@
+//
+//  SongsListViewPresenter.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/03/01.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import UIKit
+
+// so this classes charectoristic is defined by this protocol that it inherits from
+//
+class SongsListViewPresenter: SongListPresenterProtocol {
+    weak var view: SongsListViewProtocol?
+    var interactor: SongsListInteratorInputProtocol?
+    var wireframe: SongsListViewWireFrameProtocol?
+    
+    func viewDidLoad() {
+        view?.showLoading()
+    }
+    
+    func showSongDetail(forSong song: SongModel) {
+        wireframe?.presentSongDetailsScreen(from: view!, forSong: song)
+    }
+
+}
+
+extension SongsListViewPresenter: SongsListInteratorOutputProtocol {
+    func didRetrieveSongs(_ songs: [SongModel]) {
+        view?.hideLoading()
+        view?.showSongsList(songs: songs)
+    }
+    
+    func onError() {
+        view?.hideLoading()
+        view?.showError()
+    }
+}
