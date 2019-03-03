53f62040cce375302d14c34fdb050cae5ed787fc
Divine Dube <216004854@student.uj.ac.za>
2019-03-03 11:00:11 +0200

    VIPERISED

---
+++ Songhound/Entities/SongModelResponse.swift
@@ -0,0 +1,9 @@
+//
+//  SongModelResponse.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/03/03.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import Foundation
---
+++ Songhound/SongsList/DataManager/SongListRemoteDataManager.swift
@@ -0,0 +1,9 @@
+//
+//  SongListRemoteDataManager.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/03/02.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import Foundation
