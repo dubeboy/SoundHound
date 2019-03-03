353bcdff2e3180e3e0b32f328918f6b1b1881cc9
Divine Dube <216004854@student.uj.ac.za>
2019-03-03 11:00:11 +0200

    VIPERISED

---
+++ Songhound/SongsList/WireFrame/SongsListWireFrame.swift
@@ -0,0 +1,30 @@
+//
+//  SongsListWireFrame.swift
+//  Songhound
+//
+//  Created by Divine Dube on 2019/03/02.
+//  Copyright © 2019 Divine Dube. All rights reserved.
+//
+
+import UIKit
+
+class SongsListWireFrame: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
