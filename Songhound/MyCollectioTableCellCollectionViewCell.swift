02690f75108fb766cd58066deb8fdcbdf6730c6c
Divine Dube <216004854@student.uj.ac.za>
2019-03-18 10:42:30 +0200

    Fix a few crazy issues

--- Songhound/MyCollectioTableCellCollectionViewCell.swift
+++ Songhound/MyCollectioTableCellCollectionViewCell.swift
@@ -9,17 +9,17 @@
 import UIKit
 
 class MyCollectioTableCellCollectionViewCell: UICollectionViewCell {
-    weak var lblSong : UILabel!
-    weak var lblArtistName : UILabel!
-    weak var lblGenre : UILabel!
- 
+    weak var lblSong: UILabel!
+    weak var lblArtistName: UILabel!
+    weak var lblGenre: UILabel!
+
     override init(frame: CGRect) {
         super.init(frame: frame)
-       
+
         let lblSong = UILabel(frame: .zero)
         lblSong.translatesAutoresizingMaskIntoConstraints = false
         self.contentView.addSubview(lblSong)
-        
+
         NSLayoutConstraint.activate([
             lblSong.topAnchor.constraint(equalTo: self.contentView.topAnchor),
             lblSong.bottomAnchor.constraint(equalTo: self.contentView.bottomAnchor),
@@ -27,30 +27,24 @@ class MyCollectioTableCellCollectionViewCell: UICollectionViewCell {
             lblSong.trailingAnchor.constraint(equalTo: self.contentView.trailingAnchor)
         ])
         self.lblSong = lblSong
-        
+
         self.contentView.backgroundColor = .lightGray
         self.lblSong.textAlignment = .center
     }
-    
+
 
     required init?(coder aDecoder: NSCoder) {
         super.init(coder: aDecoder)
         print("Interface builder is not supported")
         return
     }
-    
+
     override func awakeFromNib() {
         print("Interface builder is not supported")
         return
     }
-    
+
     override func prepareForReuse() {
         self.lblSong.text = nil
     }
-    
-    
-    
-    
-    
-    
 }
