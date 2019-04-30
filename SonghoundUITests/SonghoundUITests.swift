a7b82ba3a263377a0e22a285d50d66531316cb20
Divine Dube <216004854@student.uj.ac.za>
2019-04-30 09:49:35 +0200

    Try testing for the last time

--- SonghoundUITests/SonghoundUITests.swift
+++ SonghoundUITests/SonghoundUITests.swift
@@ -15,7 +15,7 @@ class SonghoundUITests: XCTestCase {
     override func setUp() {
         // Put setup code here. This method is called before the invocation of each test method in the class.
         // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
+        continueAfterFailure = true
 
         // UI tests must launch the application that they test. Doing this in setup will make sure it happens
         // for each test method.
@@ -45,6 +45,51 @@ class SonghoundUITests: XCTestCase {
         XCTWaiter.wait(for: [XCTestExpectation(description:"")], timeout: 5)
         XCTAssert(true)
         
+    }
+    
+    
+    func testMainScreen() {
+        XCTAssert(app.navigationBars["🔥Hot Songs & Artists🔥"].exists)
+    }
+    
+    
+    func testSongDetailsScreen() {
+        
+        let app = XCUIApplication()
+        app.tables.staticTexts["Drive (feat. Delilah Montagu)"].tap()
+        let x =  app.otherElements.containing(.navigationBar, identifier:"Songhound.SongDetailView").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.exists
+        
+        XCTAssert(x)
+    }
+    
+    func testClickSongsArtists() {
+        
+        
+        let app = XCUIApplication()
+        app.otherElements.containing(.navigationBar, identifier:"🔥Hot Songs & Artists🔥").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element(boundBy: 1).children(matching: .image).element(boundBy: 0).tap()
+        app.tables.staticTexts["Shake It Off"].tap()
+        var x  = app.otherElements.containing(.navigationBar, identifier:"Songhound.SongDetailView").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.exists
+        XCTAssert(x)
+    }
+    
+    
+    func testOnClickMoreSongs() {
+        
+        let app = XCUIApplication()
+        app.buttons["More Songs"].tap()
+        let tablesQuery = app.tables
+        let searchField = tablesQuery.children(matching: .searchField).element
+        searchField.tap()
+        searchField.typeText("Bla")
+        
+        XCTWaiter.wait(for: [XCTestExpectation(description:"")], timeout: 10)
+        
+        XCUIApplication().tables.staticTexts["Blank Space"].tap()
+        
+        
+        var x =  app.otherElements.containing(.navigationBar, identifier:"Songhound.SongDetailView").children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.children(matching: .other).element.exists
+        
+        XCTAssert(x)
     }
 
 }
